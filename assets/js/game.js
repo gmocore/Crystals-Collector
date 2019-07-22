0a313f084a4c9cc67348b46f790fddbff54be714
gerritt <gmocore@gmail.com>
2019-07-22 13:34:25 -0600

    initial commit

---
+++ assets/js/game.js
@@ -0,0 +1,17 @@
+// create 4 gems
+    // randomly generate value for each gem from 1-10
+    // this changes each game
+
+// generate random point target between 19-120
+
+// create variables for wins, losses, points, DOM display
+
+// score starts at 0, clicking gem, adds random value to score
+    // if score === target, wins++
+    // if score > target losses++
+        // win or loss triggers new game
+
+//newGame()
+    // score resets to 0
+    // gems have new random value assigned
+    // new target value created and displayed on page
